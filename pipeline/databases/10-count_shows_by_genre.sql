9e4aabd4e33b7a68070de8ba271225e580a13b42
Ardneb <ben@greiveldinger.eu>
2026-06-01 18:07:38 +0200

    Corrected 10-count_shows_by_genre.sql

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -4,8 +4,8 @@ SELECT
     c.name AS genre,
     COUNT(DISTINCT a.id) AS number_of_shows
 FROM hbtn_0d.tv_shows a
-INNER JOIN hbtn_0d.tv_shows_genres b ON b.show_id = a.id
-INNER JOIN hbtn_0d.tv_genres c ON c.id = b.genre_id
+INNER JOIN tv_show_genres b ON b.show_id = a.id
+INNER JOIN tv_genres c ON c.id = b.genre_id
 GROUP BY c.name
 ORDER BY 2 DESC
 ;
