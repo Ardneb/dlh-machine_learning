bef7985ebc61567ad6bd3ebf6f4ff1640b300579
Ardneb <ben@greiveldinger.eu>
2026-06-01 18:05:47 +0200

    Added 10-count_shows_by_genre.sql

---
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -0,0 +1,11 @@
+-- List all genres from hbtn_0d_tvshows and 
+-- display the number of shows linked to each
+SELECT 
+    c.name as genre,
+    COUNT(DISTINCT a.id) as number_of_shows
+FROM hbtn_0d.tv_shows a
+INNER JOIN hbtn_0d.tv_shows_genres b on b.show_id = a.id
+INNER JOIN hbtn_0d.tv_genres c on c.id = b.genre_id
+GROUP BY c.name
+ORDER BY 2 DESC
+;
