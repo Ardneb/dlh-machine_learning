14190faccc0e2da7d34a5c5fc244fd87ca744c2b
Ardneb <ben@greiveldinger.eu>
2026-06-01 18:31:53 +0200

    Corrected 12-rating_genres.sql

--- pipeline/databases/12-rating_genres.sql
+++ pipeline/databases/12-rating_genres.sql
@@ -4,7 +4,7 @@ SELECT
     c.name,
     SUM(d.rate) AS rating
 FROM tv_shows a
-INNER JOIN tv_shows_genres b ON b.show_id = a.id
+INNER JOIN tv_show_genres b ON b.show_id = a.id
 INNER JOIN tv_genres c ON c.id = b.genre_id
 INNER JOIN tv_show_ratings d ON d.show_id = a.id
 GROUP BY c.name
