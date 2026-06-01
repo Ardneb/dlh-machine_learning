bf84ff3610492f773cd45833a639b75b5489c2de
Ardneb <ben@greiveldinger.eu>
2026-06-01 17:57:53 +0200

    Added 8-genre_id_by_show.sql

---
+++ pipeline/databases/8-genre_id_by_show.sql
@@ -0,0 +1,9 @@
+-- lists all shows contained in hbtn_0d_tvshows
+-- that have at least one genre linked
+SELECT 
+    a.title,
+    b.genre_id
+FROM tv_shows a
+INNER JOIN tv_show_genres b on b.show_id = a.id
+ORDER BY 1,2
+;
