3ce17f5eb9df8a77a5e8430ec146e53445b12801
Ardneb <ben@greiveldinger.eu>
2026-06-23 12:51:40 +0000

    Corrected 1-minor.py

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
 """Calculate the minor matrix of a matrix"""
-
-
-def determinant(matrix):
-    """Function calculates the determinant of a matrix"""
-    det = 0
-    for i in range(len(matrix)):
-        minor = [row[:i] + row[i + 1:]
-                 for row in matrix[1:]]
-        det += ((-1) ** i) * matrix[0][i] * determinant(minor)
-    return det
+determinant = __import__('0-determinant').determinant
 
 
 def minor(matrix):
@@ -19,7 +10,9 @@ def minor(matrix):
     for row in matrix:
         if not isinstance(row, list):
             raise TypeError("matrix must be a list of lists")
-    if len(matrix) != len(matrix[0]) or matrix == [[]]:
+    if matrix == [[]] or matrix == []:
+        raise ValueError("matrix must be a non-empty square matrix")
+    if len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a non-empty square matrix")
     minormatrix = []
     for i in range(len(matrix)):
