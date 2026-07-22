d8281e606c2aba9fbb51572b1438bce97efaa8df
Ardneb <ben@greiveldinger.eu>
2026-07-22 16:35:06 +0000

    Updated README.md and exponential.py

--- math/probability/README.md
+++ math/probability/README.md
@@ -8,3 +8,5 @@ Calculate the value of the CDF for a given number of "successes"
 Create a class Exponential that represents an exponential distribution
 ## 4. Exponential PDF
 Calculate the value of the PDF for a given time period
+## 5. Exponential CDF
+Calculate the value of the CDF for a given time period
--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -28,3 +28,11 @@ class Exponential:
         else:
             e = 2.7182818285
             return self.lambtha * e ** (-self.lambtha * x)
+
+    def cdf(self, x):
+        """Calculate the value of the CDF"""
+        if x < 0:
+            return 0
+        else:
+            e = 2.7182818285
+            return 1 - e ** (-self.lambtha * x)
