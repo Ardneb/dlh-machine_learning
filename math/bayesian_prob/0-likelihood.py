80c2b995bee2634a77bb437254d90af661dff422
Ardneb <ben@greiveldinger.eu>
2026-08-02 10:24:44 +0000

    Added README.md and 0-likelihood.py

---
+++ math/bayesian_prob/0-likelihood.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import numpy as np
+
+"""Calculate the likelihood"""
+
+
+def likelihood(x, n, P):
+    """
+    Function calculates the likelihood
+    with the given data of developing
+    severe side effects
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    elif not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is "
+                         "greater than or equal to 0")
+    elif x > n:
+        raise ValueError("x cannot be greater than n")
+    elif not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    elif np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    x_fact = np.math.factorial(x)
+    n_fact = np.math.factorial(n)
+    x_n_fact = np.math.factorial(n - x)
+    return (n_fact / (x_fact * x_n_fact) *
+            P ** x * (1 - P) ** (n - x))
---
+++ math/bayesian_prob/README.md
@@ -0,0 +1,2 @@
+## 0. Likelihood
+Write a function def likelihood(x, n, P): that calculates the likelihood of obtaining this data given various hypothetical probabilities of developing severe side effects
