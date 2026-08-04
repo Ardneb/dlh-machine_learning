c494a7360439bd356d4a7a27e8079f06fe9a434c
Ardneb <ben@greiveldinger.eu>
2026-08-04 17:05:39 +0000

    Updated README.md and added 1-intersection.py

---
+++ math/bayesian_prob/1-intersection.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Calculate the intersection"""
+import numpy as np
+
+
+def intersection(x, n, P, Pr):
+    """
+    Function calculates the intersection
+    of obtaining this data with the various
+    hypothetical probabilities
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    elif not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is "
+                         "greater than or equal to 0")
+    elif x > n:
+        raise ValueError("x cannot be greater than n")
+    elif not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    elif np.any((P < 0) | (P > 1)) or np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError(f"All values in {P} must be in the range [0, 1]")
+    elif not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
--- math/bayesian_prob/README.md
+++ math/bayesian_prob/README.md
@@ -1,2 +1,4 @@
 ## 0. Likelihood
 Write a function def likelihood(x, n, P): that calculates the likelihood of obtaining this data given various hypothetical probabilities of developing severe side effects
+## 1. Intersection
+Based on 0-likelihood.py, write a function def intersection(x, n, P, Pr): that calculates the intersection of obtaining this data with the various hypothetical probabilities
