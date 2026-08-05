70ce2e4c484406536b6f5fb95740d50f0082102d
Ardneb <ben@greiveldinger.eu>
2026-08-05 17:27:47 +0000

    Added 3-posterior.py and updated README.md

---
+++ math/bayesian_prob/3-posterior.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""calculates the posterior probability"""
+import numpy as np
+
+
+def posterior(x, n, P, Pr):
+    """
+    Calculate the posterior probability for
+    the various hypothetical probabilities
+    of developing severe side effects given the data
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    elif not isinstance(x, int) or x < 0:
+        raise ValueError("x must be an integer that is "
+                         "greater than or equal to 0")
+    elif x > n:
+        raise ValueError("x cannot be greater than n")
+    elif not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    elif not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    elif np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    elif np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    elif not np.isclose(np.sum(Pr), 1):
+        raise ValueError("Pr must sum to 1")
+    x_fact = np.math.factorial(x)
+    n_fact = np.math.factorial(n)
+    x_n_fact = np.math.factorial(n - x)
+    likelihood = (n_fact / (x_fact * x_n_fact) *
+                  P ** x * (1 - P) ** (n - x))
+    intersection = likelihood * Pr
+    marginal = intersection.sum()
+    return intersection / marginal
--- math/bayesian_prob/README.md
+++ math/bayesian_prob/README.md
@@ -4,3 +4,5 @@ Write a function def likelihood(x, n, P): that calculates the likelihood of obta
 Based on 0-likelihood.py, write a function def intersection(x, n, P, Pr): that calculates the intersection of obtaining this data with the various hypothetical probabilities
 ## 2. Marginal Probability
 Based on 1-intersection.py, write a function def marginal(x, n, P, Pr): that calculates the marginal probability of obtaining the data
+## 3. Posterior
+Based on 2-marginal.py, write a function def posterior(x, n, P, Pr): that calculates the posterior probability for the various hypothetical probabilities of developing severe side effects given the data
