7b02c5c02af122a92fd950c1c0b45a2240df6d63
Ardneb <ben@greiveldinger.eu>
2026-08-05 17:03:29 +0000

    Updated 1-intersection.py

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -21,9 +21,9 @@ def intersection(x, n, P, Pr):
     elif not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
         raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
     elif np.any((P < 0) | (P > 1)):
-        raise ValueError(f"All values in {P} must be in the range [0, 1]")
+        raise ValueError("All values in P must be in the range [0, 1]")
     elif np.any((Pr < 0) | (Pr > 1)):
-        raise ValueError(f"All values in {Pr} must be in the range [0, 1]")
+        raise ValueError("All values in Pr must be in the range [0, 1]")
     elif not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
     x_fact = np.math.factorial(x)
