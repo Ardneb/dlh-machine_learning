151c95335adae164e1db9a776db7b68c43e19027
Ardneb <ben@greiveldinger.eu>
2026-08-05 18:25:12 +0000

    Added README.md and 0-mean_cov.py

---
+++ math/multivariate_prob/0-mean_cov.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Calculate the mean and covariance"""
+import numpy as np
+
+
+def mean_cov(X):
+    """
+    Function calculates the mean
+    and covariance of a data set
+    """
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+    mean = np.mean(X, axis=0).reshape(1, -1)
+    deviation = X - mean
+    cov = deviation.T @ deviation / (X.shape[0] - 1)
+    return mean, cov
