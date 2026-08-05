bbc482dc8cc9cb3ffdb15da0fdf3cdf5e182f520
Ardneb <ben@greiveldinger.eu>
2026-08-05 16:58:04 +0000

    Updated 1-intersection.py

--- math/bayesian_prob/1-intersection.py
+++ math/bayesian_prob/1-intersection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Calculate the intersection"""
 import numpy as np
+likelihood = __import__("0-likelihood").likelihood
 
 
 def intersection(x, n, P, Pr):
@@ -18,7 +19,12 @@ def intersection(x, n, P, Pr):
         raise ValueError("x cannot be greater than n")
     elif not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
-    elif np.any((P < 0) | (P > 1)) or np.any((Pr < 0) | (Pr > 1)):
+    elif not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    elif np.any((P < 0) | (P > 1)):
         raise ValueError(f"All values in {P} must be in the range [0, 1]")
+    elif np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError(f"All values in {Pr} must be in the range [0, 1]")
     elif not np.isclose(np.sum(Pr), 1):
         raise ValueError("Pr must sum to 1")
+    return likelihood(x, n, P) * Pr
